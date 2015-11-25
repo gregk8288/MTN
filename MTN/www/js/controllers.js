5c0429580e390fd9ec4317e5a6ba61d76a8e39ae
Ruaan Erasmus <ruaan@tangentsolutions.co.za>
2015-11-25 13:12:03 +0200

    setup sass

---
+++ MTN/www/js/controllers.js
@@ -0,0 +1,18 @@
+angular.module('app.controllers', [])
+  
+.controller('loginCtrl', function($scope) {
+
+})
+   
+.controller('pageCtrl', function($scope) {
+
+})
+   
+.controller('signupCtrl', function($scope) {
+
+})
+   
+.controller('mTNAcadamyCtrl', function($scope) {
+
+})
+ 
---
+++ MTN/www/js/routes.js
@@ -0,0 +1,55 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+      
+        
+    .state('login', {
+      url: '/email',
+      templateUrl: 'templates/login.html',
+      controller: 'loginCtrl'
+    })
+        
+      
+    
+      
+        
+    .state('page', {
+      url: '/social',
+      templateUrl: 'templates/page.html',
+      controller: 'pageCtrl'
+    })
+        
+      
+    
+      
+        
+    .state('signup', {
+      url: '/page3',
+      templateUrl: 'templates/signup.html',
+      controller: 'signupCtrl'
+    })
+        
+      
+    
+      
+        
+    .state('mTNAcadamy', {
+      url: '/home',
+      templateUrl: 'templates/mTNAcadamy.html',
+      controller: 'mTNAcadamyCtrl'
+    })
+        
+      
+    ;
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/email');
+
+});

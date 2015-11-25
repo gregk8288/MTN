ac16698e47e978eeb7352fd74e07bc80611e5238
djo bukata <djobukata@gmail.com>
2015-11-25 23:35:51 +0200

    created img folder and designed the images

--- MTN/www/js/routes.js
+++ MTN/www/js/routes.js
@@ -7,46 +7,34 @@ angular.module('app.routes', [])
   // Set up the various states which the app can be in.
   // Each state's controller can be found in controllers.js
   $stateProvider
-    
-      
-        
+
+
+
     .state('login', {
       url: '/email',
       templateUrl: 'templates/login.html',
       controller: 'loginCtrl'
     })
-        
-      
-    
-      
-        
+
     .state('page', {
       url: '/social',
       templateUrl: 'templates/page.html',
       controller: 'pageCtrl'
     })
-        
-      
-    
-      
-        
+
     .state('signup', {
       url: '/page3',
       templateUrl: 'templates/signup.html',
       controller: 'signupCtrl'
     })
-        
-      
-    
-      
-        
+
     .state('mTNAcadamy', {
       url: '/home',
       templateUrl: 'templates/mTNAcadamy.html',
       controller: 'mTNAcadamyCtrl'
     })
-        
-      
+
+
     ;
 
   // if none of the above states are matched, use this as the fallback

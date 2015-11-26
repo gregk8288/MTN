2153e8da5868b51cd4d978b5041eabcc2b8b240f
djo bukata <djobukata@gmail.com>
2015-11-26 12:14:29 +0200

    linked login to subscription page

--- MTN/www/js/controllers.js
+++ MTN/www/js/controllers.js
@@ -1,18 +1,21 @@
 angular.module('app.controllers', [])
-  
-.controller('loginCtrl', function($scope) {
+
+.controller('loginCtrl', function($scope, $state) {
+
+  $scope.goTosignup = function () {
+    $state.go('signup');
+  }
 
 })
-   
+
 .controller('pageCtrl', function($scope) {
 
 })
-   
+
 .controller('signupCtrl', function($scope) {
 
 })
-   
+
 .controller('mTNAcadamyCtrl', function($scope) {
 
 })
- 

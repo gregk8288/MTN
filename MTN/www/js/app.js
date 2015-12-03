657d988e26a1b462ec37626ba6fbcdad455ed705
djo bukata <djobukata@gmail.com>
2015-12-03 15:49:55 +0200

    added splash

--- MTN/www/js/app.js
+++ MTN/www/js/app.js
@@ -5,9 +5,9 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.services', 'app.directives', 'tabSlideBox','firebase'])
+angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.services', 'app.directives', 'tabSlideBox','firebase','ngCordova'])
 
-.run(function($ionicPlatform) {
+.run(function($ionicPlatform, $cordovaSplashscreen) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -19,4 +19,7 @@ angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.services',
       StatusBar.styleDefault();
     }
   });
+  setTimeout(function() {
+    $cordovaSplashscreen.hide()
+  }, 5000)
 })

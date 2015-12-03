f0bc2a0b73e217bd4796cafacdc5e239922434d8
djo bukata <djobukata@gmail.com>
2015-12-03 11:49:26 +0200

    popup

--- MTN/www/js/controllers.js
+++ MTN/www/js/controllers.js
@@ -22,10 +22,24 @@ angular.module('app.controllers', ['firebase'])
   }
 })
 
-.controller('mTNAcadamyCtrl', function($scope, $ionicPopover, $state) {
+.controller('mTNAcadamyCtrl', function($scope, $ionicPopover, $state, $ionicPopup) {
 
   $scope.goBack = function(){
     $state.go('profile');
+  };
+
+  $scope.showConfirm = function() {
+    var confirmPopup = $ionicPopup.confirm({
+      title: 'Add Excel training',
+      template: 'By adding this training to your profile you will receive any notifications / updates send to this group'
+    });
+    confirmPopup.then(function(res) {
+      if(res) {
+        console.log('You are sure');
+      } else {
+        console.log('You are not sure');
+      }
+    });
   };
 
 })

c1dcffe3ab4422fbcd2e09061cfe0af296abd029
Ruaan Erasmus <ruaan@tangentsolutions.co.za>
2015-12-04 11:43:31 +0200

    remving db

--- MTN/www/js/controllers.js
+++ MTN/www/js/controllers.js
@@ -1,16 +1,65 @@
 angular.module('app.controllers', ['firebase'])
 
-.controller('loginCtrl', function($scope, $state) {
+.controller('loginCtrl', function($scope, $state, $rootScope, $firebase) {
 
   $scope.goTosignin = function () {
     $state.go('signin');
   }
+    
+  $scope.user = "";
+    // var ref = new Firebase("https://mtn.firebaseio.com/");
+ //
+ //    ref.createUser({
+ //      email    : "eruaanndd@gmail.com",
+ //      password : "test",
+ //        username: "ruaan"
+ //    }, function(error, userData) {
+ //      if (error) {
+ //        console.log("Error creating user:", error);
+ //      } else {
+ //        console.log("Successfully created user account with uid:", userData.uid);
+ //      }
+ //    });
+
+    
+
+    $scope.loginEmail = function(user){
+        console.log(user.email);
+       var ref = new Firebase("https://mtn.firebaseio.com/");
+ 
+      ref.authWithPassword({
+        email    : user.email,
+        password : "test"
+      }, function(error, authData) {
+        if (error) {
+          console.log("Login Failed!", error);
+        } else {
+          console.log("Authenticated successfully with payload:", authData);
+          $rootScope.uid = authData.uid;
+           $state.go('signin');
+        }
+      });
+      
+    };
 
 })
 
-.controller('profileCtrl', function($scope, $state) {
-
-  $scope.goToMtnAcademy= function () {
+.controller('profileCtrl', function($scope, $state, $rootScope, $firebase, $firebaseArray) {
+    console.log($rootScope.uid);
+    
+    var ref = new Firebase('https://mtn.firebaseio.com/');
+    var sync = $firebaseArray(ref);
+    $scope.userDetails = sync;
+    
+    $scope.userDetails.$add({
+            user: $rootScope.uid,
+            firstname:"Ruaan",
+            lastname:"Eramus",
+            email:"eruaan@gmail.com",
+            jobTitle:"senior develper"
+          });
+          
+    $scope.goToMtnAcademy= function () {
     $state.go('mTNAcadamy');
   }
 
@@ -51,7 +100,7 @@ angular.module('app.controllers', ['firebase'])
 
     /////chat part //////
 
-    var ref = new Firebase('https://hel.firebaseio.com/');
+    var ref = new Firebase('https://mtn.firebaseio.com/');
     var sync = $firebaseArray(ref);
     $scope.chats = sync;
 
